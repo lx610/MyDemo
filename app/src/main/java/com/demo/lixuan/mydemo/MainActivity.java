63a554d044fd52422def60aa1f01111bec145c72
lx <lixuan_1@163.com>
2018-05-23 17:04:45 +0800

    修改新增多渠道测试

--- app/src/main/java/com/demo/lixuan/mydemo/MainActivity.java
+++ app/src/main/java/com/demo/lixuan/mydemo/MainActivity.java
@@ -18,6 +18,7 @@ import com.demo.lixuan.mydemo.DemoActivity.OOMExampleACitivity;
 import com.demo.lixuan.mydemo.DemoActivity.ProcessActivity;
 import com.demo.lixuan.mydemo.DemoActivity.RetrofitIntorcepterActivity;
 import com.demo.lixuan.mydemo.DemoActivity.StringAndTextViewActivity;
+import com.demo.lixuan.mydemo.gradleSetting.GradleSetMutlApkActivity;
 import com.demo.lixuan.mydemo.thread.ThreadActivity;
 import com.demo.lixuan.mydemo.DemoActivity.ViewAndImageBannerBarActivity;
 import com.demo.lixuan.mydemo.DemoActivity.WidgtActivity;
@@ -62,6 +63,7 @@ public class MainActivity extends BaseActivity {
         mLlBtContainer.addView(generateBtToStartActivity(new AnnotationTestActivity()));
         mLlBtContainer.addView(generateBtToStartActivity(new ProcessActivity()));
         mLlBtContainer.addView(generateBtToStartActivity(new KotLinActivity()));
+        mLlBtContainer.addView(generateBtToStartActivity(new GradleSetMutlApkActivity()));
 
     }
 
---
+++ app/src/main/java/com/demo/lixuan/mydemo/gradleSetting/GradleSetMutlApkActivity.java
@@ -0,0 +1,54 @@
+package com.demo.lixuan.mydemo.gradleSetting;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.demo.lixuan.mydemo.BuildConfig;
+import com.demo.lixuan.mydemo.R;
+import com.demo.lixuan.mydemo.Utils.UiUtils;
+import com.demo.lixuan.mydemo.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Administrator on 2018/5/22.
+ */
+
+public class GradleSetMutlApkActivity extends BaseActivity {
+    @BindView(R.id.ll_container)
+    LinearLayout mLlContainer;
+
+    @Override
+    public int getLayoutResId() {
+        return R.layout.activity_only_linearlayout;
+    }
+
+    @Override
+    public void initView() {
+        mLlContainer.addView(generateTextButton(BuildConfig.APPLICATION_ID + "", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UiUtils.makeText(BuildConfig.webUrl + "");
+            }
+        }));
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+}
