5e9fafb168eafbd33c359e44a995b632f8b343b6
lx <lixuan_1@163.com>
2020-05-18 16:16:26 +0800

    新增可点击展开的textView

---
+++ app/src/main/java/com/demo/lixuan/mydemo/widgt/textView/textView/mode/LinkType.java
@@ -0,0 +1,13 @@
+package com.demo.lixuan.mydemo.widgt.textView.textView.mode;
+
+//定义类型的枚举类型
+public enum LinkType {
+    //普通链接
+    LINK_TYPE,
+    //@用户
+    MENTION_TYPE,
+    //电话号码
+    PHONE_TYPE,
+    //自定义规则
+    SELF
+}
